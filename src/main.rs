d81bbf2e40829db69e8701d16f920393912bc2ac
Lucas Holten <lhc.holten@gmail.com>
2022-12-03 15:39:25 +0100

    tweak parameters

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 #![feature(array_zip)]
+#![feature(iter_array_chunks)]
 
 use std::{fs::File, io::BufReader};
 
@@ -6,18 +7,54 @@ use dfdx::prelude::*;
 use jpeg_decoder as jpeg;
 use rand::{rngs::StdRng, SeedableRng};
 use show_image::{create_window, ImageInfo, ImageView};
+// use vecmath::{vec2_add, vec2_mul, vec2_scale, vec2_sub};
 
 use crate::hashtable::HashTable;
 
+mod box_slice;
 mod hashtable;
 mod mlp;
 
+// pub fn render_layer<const L: usize, const T: usize>(
+//     table: HashTable<L, T>,
+//     layer: usize,
+//     feature: usize,
+// ) -> Box<[[u8; 2]; 256 * 256]> {
+//     let coords = (0..256).flat_map(|y| (0..256).map(move |x| [x, y]));
+
+//     let values: Box<[[f32; 2]; 256 * 256]> = coords
+//         .array_chunks()
+//         .flat_map(|coords| {
+//             let output: Tensor2D<256, 2, NoneTape> = table.get(&coords, layer);
+//             *output.data()
+//         })
+//         .collect::<Box<[[f32; 2]]>>()
+//         .try_into()
+//         .unwrap();
+//     let total = values.iter().copied().fold([0., 0.], vec2_add);
+//     let avg = vec2_scale(total, 1. / (256. * 256.));
+//     values.iter_mut().for_each(|v| *v = vec2_sub(*v, avg));
+//     let total_var = values
+//         .iter()
+//         .copied()
+//         .map(|x| vec2_mul(x, x))
+//         .fold([0., 0.], vec2_add);
+//     let var = vec2_scale(total_var, 1. / (256. * 256.));
+//     // values.into_iter()
+
+//     // let total_squared = values.iter().sum();
+
+//     // values.iter_mut().for_each(|v| *v /= avg)
+// }
+
 #[show_image::main]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut table = HashTable::<16, { 1 << 14 }>::default();
+    let mut table = HashTable::<7, { 1 << 10 }>::default();
 
     let mut rng = StdRng::seed_from_u64(0);
     table.reset_params(&mut rng);
+
+    println!("{:?}", table.grid_sizes());
 
     let file = File::open("SIPI_Jelly_Beans_4.1.07.tiff.jpg").expect("failed to open file");
     let mut decoder = jpeg::Decoder::new(BufReader::new(file));
@@ -25,7 +62,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let image = ImageView::new(ImageInfo::rgb8(256, 256), &pixels);
     let window = create_window("image", Default::default())?;
-    window.set_image("image-001", image)?;
+    window.set_image("reference", image)?;
 
     let pixels: Box<[[f32; 3]; 256 * 256]> = pixels
         .chunks_exact(3)
@@ -79,6 +116,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             .collect::<Vec<u8>>();
 
         let image = ImageView::new(ImageInfo::rgb8(256, 256), &output);
-        window.set_image("image-001", image)?;
+        window.set_image("output", image)?;
     }
 }
--- src/mlp.rs
+++ src/mlp.rs
@@ -4,7 +4,7 @@ use dfdx::{
 };
 use rand::distributions::Uniform;
 
-pub type Mlp<const L: usize> = (ReLU, Linear<64, 64>, ReLU, Linear<64, 3>, Sigmoid);
+pub type Mlp<const L: usize> = (ReLU, Linear<32, 32>, ReLU, Linear<32, 3>, Sigmoid);
 
 #[derive(Clone)]
 pub struct ConcatLinear<const L: usize, const O: usize> {
